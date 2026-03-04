4ed5fffd2f724482a21c2f51724732f622ba083a
IbrahimaCOLY13 <ibra.coly9@gmail.com>
2026-03-04 12:22:21 +0100

    Version V1.6 publiée le 04/03/2026

--- VersionV1.6/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Package modules contenant les utilitaires du plugin HU_tools.
-"""
-
-# Ce fichier peut rester vide, sa présence suffit pour que Python
-# reconnaisse le dossier comme un package
